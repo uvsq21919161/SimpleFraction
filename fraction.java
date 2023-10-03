061820a76e340c4e2ab0f7dd3734fe27ae24f3be
Thanushan <thanushan.pirabakaran@ens.uvsq.fr>
2023-10-03 12:02:40 +0200

    Fraction

---
+++ fraction.java
@@ -0,0 +1,16 @@
+class fraction {
+    @Override
+    public String toString() {
+        System.out.println("Je suis une fraction.");
+        return "";
+    }
+}
+
+class main {
+
+    public static void main ( String [] args) {
+        fraction MaFraction = new fraction();
+        MaFraction.toString();    
+    }
+    
+}
